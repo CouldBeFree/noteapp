e1cf9baf16d2e314a16d483150628d2398e09e5b
Explorer <propokerstar90@gmail.com>
2018-08-27 21:47:56 +0300

    Initial commit

---
+++ src/components/note.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+      Note
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "note"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/noteEditor.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      <textarea cols="30" rows="10"></textarea>
+      <button>Add</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "noteEditor"
+    }
+</script>
+
+<style scoped>
+
+</style>
